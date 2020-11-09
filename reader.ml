df75afc03b7e0d3e3b9d93fb0d50aba0465fcc17
comp211 <izhakmo@post.bgu.ac.il>
2020-11-09 15:50:44 +0200

    building a theoretical struct

--- reader.ml
+++ reader.ml
@@ -41,6 +41,68 @@ let normalize_scheme_symbol str =
   else Printf.sprintf "|%s|" str;;
 
 
+(* if we find # ==> we need to check for a boolean insensative or char*))
+
+let _#_ = (char '#');;
+
+let _boolOrBackSlash x = match x with
+    | 'f' -> Bool(false)
+    | 't' -> Bool(true)
+    | _ -> raise X_not_yet_implemented;;
+
+let digit = range '0' '9';; 
+
+(* TODO ADD SIGN plux or minus before*)
+
+let tok_num_=
+  let digits= plus digit in 
+  pack digits(fun (ds) -> Num (int_of_string(list_to_string ds)));;
+
+
+
+let _tokenize_num x = match x with
+  | '/' -> String("TODO FRAC and denominator")
+  | '.' -> String("TODO float")
+  | _ -> String("TODO FRAC divided by 1");;
+
+  let dot = '.';; 
+
+
+let lowerCase = range 'a' 'z';;
+let upperCase = range 'A' 'Z';;
+let punctuation = 
+  | '!' | '$' | '^' | '*' | '-' | '_' 
+  | '=' | '+' | '<' | '>' | '/' | '?';;
+
+let dots = '.' | ',';;
+
+(*let _symbolToken = *)
+
+(* let string_meta_char = '\' | '"' | 't' | 'f' | 'n' | 'r'; *)
+
+let _tokenize_meta_char x = match x with 
+  | '\\' -> Char(char '\\') 
+  | '\"' -> Char(char '\"')
+  | '\t' -> Char(char '\t')
+  | '\f' -> Char(char '\f')
+  | '\n' -> Char(char '\n')
+  | '\r' -> Char(char '\r')
+  | _ -> raise X_not_yet_implemented;;
+
+let _tokenize_named_char x = match x with
+  | "nul" -> Char(0) 
+  | "newline" -> Char(10) 
+  | "return" -> Char(13) 
+  | "tab" -> Char(9) 
+  | "formfeed" -> Char(12) 
+  | "space" -> Char(32)
+  | _ -> (_tokenize_visible_char x);;
+
+let _tokenize_visible_char x = match x with
+  | char(c) and (c > 32) -> Char(char x)
+  | _ -> raise X_not_yet_implemented;;
+
+
 let read_sexprs string = raise X_not_yet_implemented;;
   
 end;; (* struct Reader *)
