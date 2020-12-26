97208b899def3bc11dbace82d90d74d54b2c7add
izhakmo <izhakmo@post.bgu.ac.il>
2020-12-26 12:49:44 +0200

    if we understood right - fvar is correct TODO be sure about constant Symbol, pair, and add closure to consts

--- code-gen.ml
+++ code-gen.ml
@@ -15,7 +15,7 @@ module type CODE_GEN = sig
   val map_flatten_func : 'a list list -> 'a list -> 'a list
   val procces_const : expr' -> constant list
   val remove_duplicates_func : 'a list -> 'a list 
-  val allocate_mem_func : constant list -> (constant * int * string) list 
+  val allocate_mem_func : constant list -> (constant * (int * string)) list 
   
   (* This signature assumes the structure of the constants table is
      a list of key-value pairs:
@@ -34,6 +34,10 @@ module type CODE_GEN = sig
      - The values are the offsets from the base fvars_table address in bytes
      For example: [("boolean?", 0)]
    *)  
+
+
+  val procces_fvar : expr' -> string list
+  val allocate_mem_fvar_func : 'a list -> ('a * int) list
   val make_fvars_tbl : expr' list -> (string * int) list
 
   (* If you change the types of the constants and fvars tables, you will have to update
@@ -124,27 +128,100 @@ let allocate_mem_func arr_without_dups =
           caten_head head tail res index
 
   and caten_head hd arr res index = match hd with
-  | Void ->               (allocate_mem arr (res@[(Void, index, "MAKE_VOID")]) (index + 1))
-  | Sexpr(Nil) ->         (allocate_mem arr (res@[(Sexpr(Nil), index, "MAKE_NIL")]) (index + 1))
-  | Sexpr(Bool false) ->  (allocate_mem arr (res@[(Sexpr(Bool false), index, "MAKE_BOOL(0)")]) (index + 2))
-  | Sexpr(Bool true) ->   (allocate_mem arr (res@[(Sexpr(Bool true), index, "MAKE_BOOL(1)")]) (index + 2))
-  | Sexpr(Char(c1))->     (allocate_mem arr (res@[(hd, index, "MAKE_CHAR========================TODO")]) (index + 2))
+  | Void ->               (allocate_mem arr (res@[(Void, (index, "MAKE_VOID"))]) (index + 1))
+  | Sexpr(Nil) ->         (allocate_mem arr (res@[(Sexpr(Nil), (index, "MAKE_NIL"))]) (index + 1))
+  | Sexpr(Bool false) ->  (allocate_mem arr (res@[(Sexpr(Bool false), (index, "MAKE_BOOL(0)"))]) (index + 2))
+  | Sexpr(Bool true) ->   (allocate_mem arr (res@[(Sexpr(Bool true), (index, "MAKE_BOOL(1)"))]) (index + 2))
+  | Sexpr(Char(c1))->     (allocate_mem arr (res@[(hd, (index,( String.concat "" ["MAKE_CHAR(";Char.escaped c1;")"])))]) (index + 2))
   | Sexpr(Number(Float f1)) ->
-                          (allocate_mem arr (res@[(hd, index, "MAKE_FLOAT========================TODO")]) (index + 9))
+                          (allocate_mem arr (res@[(hd, (index,( String.concat "" ["MAKE_FLOAT(";(string_of_float f1);")"])))]) (index + 9))
   | Sexpr(Number(Fraction (n1, d1))) ->
-                          (allocate_mem arr (res@[(hd, index, "MAKE_FRAC========================TODO")]) (index + 17))
-  | Sexpr(String(s1)) ->  (allocate_mem arr (res@[(hd, index, "MAKE_STRING========================TODO")]) (index + 9 + (String.length s1)))
-  | Sexpr(Symbol(s1)) ->  (allocate_mem arr (res@[(hd, index, "MAKE_SYMBOL========================TODO")]) (index + 9))
+                          (allocate_mem arr (res@[(hd, (index,( String.concat "" ["MAKE_FRAC(";string_of_int n1;"/" ;string_of_int d1; ")"])))]) (index + 17))
+  | Sexpr(String(s1)) ->  (allocate_mem arr (res@[(hd, (index,( String.concat "" ["MAKE_LITERAL_STRING(";s1;")"])))]) (index + 9 + (String.length s1)))
+  | Sexpr(Symbol(s1)) ->  (allocate_mem arr (res@[(hd, (index,( String.concat "" ["MAKE_SYMBOL(";s1;")"])))]) (index + 9))
   | Sexpr(Pair(car1, cdr1)) ->
-                          (allocate_mem arr (res@[(hd, index, "MAKE_PAIR========================TODO")]) (index + 17))
+                          (allocate_mem arr (res@[(hd, (index,( String.concat "" ["MAKE_PAIR(";"TODO ========";")"])))]) (index + 17))
   in
   allocate_mem arr_without_dups [] 0 ;;
   
 
       
-  let make_consts_tbl asts = raise X_not_yet_implemented;;
+  (* let make_consts_tbl asts = (allocate_mem_func (remove_duplicates_func (procces_const asts) ));; *)
+  let make_consts_tbl asts = (allocate_mem_func (remove_duplicates_func ( map_flatten_func  (List.map procces_const asts) []) ));;
+  
+  (* let make_consts_tbl asts = raise X_not_yet_implemented;; *)
 
-  let make_fvars_tbl asts = raise X_not_yet_implemented;;
+
+
+
+
+
+
+
+
+  let procces_fvar expr = 
+    let rec fvar expr arr = match expr with
+      | Const'(s1)->          arr
+      | Var'(VarFree v1) ->   arr@[v1]
+      | Var'(v1) ->           arr
+      | Box'(v1)->            arr
+      | BoxGet'(v1)->         arr
+      | BoxSet'(v1,e1) ->     (fvar e1 arr)
+      | If'(t1, th1, el1)->   (fvar t1 arr) @ (fvar th1 []) @ (fvar el1 [])
+      | Seq'(l1) ->
+                              let apply_fvar_with_map single = fvar single [] in
+                              let map_seq = List.map apply_fvar_with_map l1 in
+                              let flattened = (map_flatten_func map_seq []) in
+                              arr@flattened
+      | Or'(l1) ->
+                              let apply_fvar_with_map single = fvar single [] in
+                              let map_or = List.map apply_fvar_with_map l1 in
+                              let flattened = (map_flatten_func map_or []) in
+                              arr@flattened
+      | Set'(var, val1) ->     (fvar val1 arr)
+      | Def'(var, val1)->      (fvar val1 arr)
+      | LambdaSimple'(vars, body)->
+                              (fvar body arr)
+      | LambdaOpt'(vars, var, body) ->
+                              (fvar body arr)
+      | Applic'(proc, args) ->
+                              let apply_fvar_with_map single = fvar single [] in
+                              let map_args = List.map apply_fvar_with_map args in
+                              let flattened = (map_flatten_func map_args []) in
+                              (fvar proc arr) @ flattened
+      | ApplicTP'(proc, args) ->
+                              let apply_fvar_with_map single = fvar single [] in
+                              let map_args = List.map apply_fvar_with_map args in
+                              let flattened = (map_flatten_func map_args []) in
+                              (fvar proc arr) @ flattened
+      (* | _ -> raise X_procces_const *)
+      in 
+      fvar expr [];;
+
+
+      let allocate_mem_fvar_func arr_without_dups = 
+        let rec allocate_mem arr res index = match arr with
+        | [] -> res
+        | _ ->  
+                let head = (List.hd arr) in
+                let tail = (List.tl arr) in 
+                caten_head head tail res index
+      
+        and caten_head hd arr res index = (allocate_mem arr (res@[(hd, (index * 8))]) (index + 1))
+        in
+        allocate_mem arr_without_dups [] 0 ;;
+
+
+
+        let make_fvars_tbl asts =  (allocate_mem_fvar_func (remove_duplicates_func ( map_flatten_func  (List.map procces_fvar asts) []) ));;
+
+
+
+
+
+
+
+  (* let make_fvars_tbl asts = raise X_not_yet_implemented;; *)
   let generate consts fvars e = raise X_not_yet_implemented;;
 end;;
 open Code_Gen;;
@@ -187,3 +264,39 @@ ApplicTP' (LambdaOpt' ([], "x", Var' (VarParam ("x", 0))),
 )
 ;;
  *)
+
+(* 
+ make_consts_tbl
+ ([(LambdaSimple' ([],
+ Seq'
+ [Const' (Sexpr (String("moshe")));
+ Const' (Sexpr (Number (Fraction(1,1))));
+ 
+   Applic' (LambdaSimple' ([], Var' (VarFree "x")), []);
+ Applic'
+ (LambdaSimple' (["x"],
+ Seq'
+ [Set' ( (VarParam ("x", 0)), Box' (VarParam ("x", 0)));
+ Const' (Sexpr (Nil));
+ Const' (Void);
+ Const' (Sexpr (Bool(true)));
+ BoxSet' (VarParam ("x", 0), Const' (Sexpr (Number (Fraction(1,1)))));
+ LambdaSimple' ([], BoxGet' (VarBound ("x", 0, 0)))]),
+ [Const' (Sexpr (Number (Fraction(2,1))))]);
+ ApplicTP' (LambdaOpt' ([], "x", Var' (VarParam ("x", 0))),
+ [Const' (Sexpr (Number (Fraction(3,1))))])]));
+
+ (LambdaSimple' (["x"],
+  Seq'
+  [Set' ( (VarParam ("x", 0)), Box' (VarParam ("x", 0)));
+    BoxGet' (VarParam ("x", 0));
+     LambdaSimple' (["x"],
+      Seq'
+       [Set' ( (VarParam ("x", 0)), Box' (VarParam ("x", 0)));
+         BoxSet' (VarParam ("x", 0), Const' (Sexpr (Number (Fraction(1,1)))));
+          LambdaSimple' ([], BoxGet' (VarBound ("x", 0, 0)))]);
+     LambdaSimple' ([],
+      BoxSet' (VarBound ("x", 0, 0), BoxGet' (VarBound ("x", 0, 0))))]))
+ 
+ ])
+  *)
